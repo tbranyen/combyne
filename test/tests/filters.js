6cae401e75ffd4d07fa817b8d2294fd68dd655ac
Tim Branyen <tim@tabdeveloper.com>
2014-02-17 03:13:37 -0500

    Added basic filters support.

--- test/tests/filters.js
+++ test/tests/filters.js
@@ -3,69 +3,89 @@ define(function(require, exports, module) {
 
   var combyne = require("lib/index");
 
-  describe("Filters", function() {});
+  describe("Filters", function() {
+    it("will not function on non filters", function() {
+      var tmpl = combyne("|| |    |");
+      var output = tmpl.render();
+
+      expect(output).to.equal("|| |    |");
+    });
+
+    it("can execute basic functions", function() {
+      var tmpl = combyne("{{test|mod6}}");
+
+      tmpl.registerFilter("mod6", function(value) {
+        return value % 6;
+      });
+
+      var output = tmpl.render({ test: 15.5 });
+
+      expect(output).to.equal("3.5");
+    });
+
+    it("can execute complex functions", function() {
+      var tmpl = combyne("testing this out {{test|reverse}}");
+
+      tmpl.registerFilter("reverse", function(value) {
+        return Array.prototype.slice.call(value).reverse().join("");
+      });
+
+      var output = tmpl.render({ test: "tart" });
+
+      expect(output).to.equal("testing this out trat");
+    });
+
+    it("can execute complex functions with arguments", function() {
+      var tmpl = combyne("{{test|concat 'lol' 'hi' 'how' 'are' 'you'}}");
+
+      tmpl.registerFilter("concat", function(value) {
+        for(var i = 1; i < arguments.length; i++) {
+          value += " " + arguments[i];
+        }
+
+        return value;
+      });
+
+      var output = tmpl.render({ test: "hmm" });
+
+      expect(output).to.equal("hmm lol hi how are you");
+    });
+
+    it("can execute an object filter", function() {
+      var tmpl = combyne("{{test|obj}}");
+
+      tmpl.registerFilter("concat", function(value) {
+        for(var i = 1; i < arguments.length; i++) {
+          value += " " + arguments[i];
+        }
+
+        return value;
+      });
+
+      tmpl.registerFilter("obj", function(value) {
+        return value["tmp"];
+      });
+
+      var output = tmpl.render({ test: { tmp:"test" } });
+
+      expect(output).to.equal("test");
+    });
+  });
 });
 
 /*
-exports.notAFilter = function( test ) {
-  test.expect(1);
-
-  var tmpl = combyne('||');
-  test.equals( tmpl.render(), '||', 'Doesn\'t attempt to render non-filters' );
-
-  test.done();
-};
-
-exports.propertyFilters = function( test ) {
-  test.expect(4);
-
-  // Basic modulus filter
-  var tmpl = combyne('{{test|mod6}}', { test: 15.5 });
-  tmpl.filters.add('mod6', function( val ) {
-    return val % 6;
-  });
-  test.equals( tmpl.render(), 3.5, 'Numerical modulus filter' );
-
-  // Slightly more complicated reverse filter
-  var tmpl2 = combyne('testing this out {{test|reverse}}', { test: 'tart' });
-  tmpl2.filters.add('reverse', function( val ) {
-    return Array.prototype.slice.call(val).reverse().join('');
-  });
-  test.equals( tmpl2.render(), 'testing this out trat', 'Reverse filter' );
- 
-  // Complex filter with arguments
-  var tmpl3 = combyne('{{test|concat "lol" "hi" "how" "are" "you"}}', { test: 'hmm' });
-  tmpl3.filters.add('concat', function( val ) {
-    for(var i = 1, len=arguments.length; i<len; i++) {
-      val += ' ' + arguments[i];
-    }
-
-    return val;
-  });
-  test.equals( tmpl3.render(), 'hmm lol hi how are you', 'Complex filter with arguments' );
-
-  // Object filter
-  var tmpl4 = combyne('{{test|obj}}', { test: {tmp:'test'} });
-  tmpl4.filters.add('obj', function( val ) {
-    return val['tmp'];
-  });
-  test.equals( tmpl4.render(), 'test', 'Testing with an object passed as val' );
-
-  test.done();
-};
-
 exports.chainedFilters = function( test ) {
   test.expect(1);
 
   // Basic chainable functions
-  var tmpl = combyne('{{test|addWord "fart"|reverse}}', { test: 'hi' });
-  tmpl.filters.add('addWord', function( val, word ) {
+  var tmpl = combyne("{{test|addWord "fart"|reverse}}", { test: "hi" });
+  tmpl.filters.add("addWord", function( val, word ) {
     return val + word;
   });
-  tmpl.filters.add('reverse', function( val ) {
-    return Array.prototype.slice.call(val).reverse().join('');
+  tmpl.filters.add("reverse", function( val ) {
+    return Array.prototype.slice.call(val).reverse().join("");
   });
-  test.equals( tmpl.render(), 'trafih', 'Basic chainable functions' );
+  test.equals( tmpl.render(), "trafih", "Basic chainable functions" );
 
   test.done();
 };
@@ -74,11 +94,11 @@ exports.typedFilters = function( test ) {
   test.expect(1);
 
   // Basic chainable functions
-  var tmpl = combyne('{{test|add 5}}', { test: 1 });
-  tmpl.filters.add('add', function( val, num ) {
+  var tmpl = combyne("{{test|add 5}}", { test: 1 });
+  tmpl.filters.add("add", function( val, num ) {
     return val + num;
   });
-  test.equals( tmpl.render(), '6', 'Work with number types' );
+  test.equals( tmpl.render(), "6", "Work with number types" );
 
   test.done();
 };
@@ -86,14 +106,14 @@ exports.typedFilters = function( test ) {
 exports.nestedFilters = function( test ) {
   test.expect(1);
 
-  var obj = { item: [ 'hi', 'you', 'own' ] };
-  var tmpl = combyne('{%each item%} {{.|render}} {%endeach%}', obj);
+  var obj = { item: [ "hi", "you", "own" ] };
+  var tmpl = combyne("{%each item%} {{.|render}} {%endeach%}", obj);
 
-  tmpl.filters.add('render', function(val) {
-    return combyne('Name: {{name}}', { name: val }).render();
+  tmpl.filters.add("render", function(val) {
+    return combyne("Name: {{name}}", { name: val }).render();
   });
 
-  test.equals( tmpl.render(), ' Name: hi  Name: you  Name: own ', 'Render templates inside filters' );
+  test.equals( tmpl.render(), " Name: hi  Name: you  Name: own ", "Render templates inside filters" );
 
   test.done();
 };
