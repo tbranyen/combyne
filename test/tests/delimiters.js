d80f2c8cd4044c7fa45d83053c8b187d4d08f551
Tim Branyen <tim@tabdeveloper.com>
2014-05-13 01:36:53 -0400

    Added back delimiters tests and removed chai.

--- test/tests/delimiters.js
+++ test/tests/delimiters.js
@@ -1,48 +1,45 @@
-//define(function(require, exports, module) {
-//  "use strict";
-//
-//  var combyne = require("../../lib/index");
-//
-//  describe("Delimiters", function() {
-//    before(function() {
-//      delete combyne.settings.delimiters.START_PROP;
-//      delete combyne.settings.delimiters.END_PROP;
-//    });
-//
-//    it("can be changed on a template", function() {
-//      combyne.settings.delimiters = {
-//        START_PROP: "[[", END_PROP: "]]"
-//      };
-//
-//      var tmpl = combyne("[[test]]", { test: "prop" });
-//
-//      var output = tmpl.render();
-//
-//      expect(output).to.equal("prop");
-//    });
-//
-//    it("can be incomplete and mixed", function() {
-//      combyne.settings.delimiters = {
-//        START_PROP: "[["
-//      };
-//
-//      var tmpl = combyne("[[test}}", { test: "prop" });
-//
-//      var output = tmpl.render();
-//
-//      expect(output).to.equal("prop");
-//    });
-//
-//    it("do not remember previous delimiters", function() {
-//      combyne.settings.delimiters = {
-//        START_PROP: "[[", END_PROP: "]]" 
-//      };
-//
-//      var tmpl = combyne("[[one]] {{two}}", { one: "prop", two: "tes" });
-//
-//      var output = tmpl.render();
-//
-//      expect(output).to.equal("prop {{two}}");
-//    });
-//  });
-//});
+define(function(require, exports, module) {
+  "use strict";
+
+  var combyne = require("../../lib/index");
+
+  describe("Delimiters", function() {
+    after(function() {
+      combyne.settings.delimiters.START_PROP = "{{";
+      combyne.settings.delimiters.END_PROP = "}}";
+    });
+
+    it("can be changed on a template", function() {
+      combyne.settings.delimiters = {
+        START_PROP: "[[", END_PROP: "]]"
+      };
+
+      var tmpl = combyne("[[test]]", { test: "prop" });
+
+      var output = tmpl.render();
+      assert.equal(output, "prop");
+    });
+
+    it("can be incomplete and mixed", function() {
+      combyne.settings.delimiters = {
+        START_PROP: "[["
+      };
+
+      var tmpl = combyne("[[test}}", { test: "prop" });
+
+      var output = tmpl.render();
+      assert.equal(output, "prop");
+    });
+
+    it("do not remember previous delimiters", function() {
+      combyne.settings.delimiters = {
+        START_PROP: "[[", END_PROP: "]]" 
+      };
+
+      var tmpl = combyne("[[one]] {{two}}", { one: "prop", two: "tes" });
+      var output = tmpl.render();
+
+      assert.equal(output, "prop {{two}}");
+    });
+  });
+});
