973375db36a622a47198e780729bade01b9faa99
Tim Branyen <tim@tabdeveloper.com>
2014-07-18 13:54:18 -0400

    Minor patch to rectify behavior in 0.4.0

--- test/tests/basic.js
+++ test/tests/basic.js
@@ -4,18 +4,25 @@ define(function(require, exports, module) {
   var combyne = require("../../lib/index");
 
   describe("Basic rendering", function() {
-    it("renders without adding unnecessary whitespace", function() {
+    it("renders without adding unnecessary leading whitespace", function() {
       var template = combyne("{%each items as item%}\n{{item}}\n{%endeach%}");
       var output = template.render({ items: [1, 2, 3] });
 
       assert.equal(output, "1\n2\n3");
     });
 
+    it("renders without adding unnecessary trailing whitespace", function() {
+      var template = combyne("{%each items as item%}\n{{item}}\n{%endeach%}\n");
+      var output = template.render({ items: [1, 2, 3] });
+
+      assert.equal(output, "1\n2\n3\n");
+    });
+
     it("renders without destroying necessary whitespace", function() {
       var template = combyne("{%each items as item%}\n\n{{item}}{%endeach%}");
       var output = template.render({ items: [1, 2, 3] });
 
-      assert.equal(output, "\n\n1\n\n2\n\n3");
+      assert.equal(output, "1\n\n2\n\n3");
     });
   });
 });
