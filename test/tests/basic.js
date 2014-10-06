1bd23edafa8b284812a93fda7762818e0745bcea
John Haley <johnh@axosoft.com>
2014-10-06 11:57:14 -0700

    Whitespaces are now only removed after an END_EXPR

--- test/tests/basic.js
+++ test/tests/basic.js
@@ -25,7 +25,7 @@ define(function(require, exports, module) {
       var template = combyne("{%each items as item%}\n{{item}}\n{%endeach%}\n");
       var output = template.render({ items: [1, 2, 3] });
 
-      assert.equal(output, "1\n2\n3\n\n");
+      assert.equal(output, "1\n2\n3\n");
     });
 
     it("renders without destroying necessary whitespace", function() {
@@ -47,7 +47,7 @@ define(function(require, exports, module) {
       var template = combyne(text);
       var output = template.render({ items: [0, 1, 2, 3] });
 
-      assert.equal(output, "\n1\n2\n3\n");
+      assert.equal(output, "1\n2\n3\n");
     });
   });
 });
